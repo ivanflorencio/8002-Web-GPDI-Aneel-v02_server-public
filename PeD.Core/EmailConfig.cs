14a51cef0d1f59c01525fef8a70a476fea115ffa
Ivan Florencio <ivanflorencio@hotmail.com>
2022-10-26 14:18:16 -0300

    NE-20: Editar e-mails enviados aos fornecedores.

--- PeD.Core/EmailConfig.cs
+++ PeD.Core/EmailConfig.cs
@@ -2,7 +2,7 @@ namespace PeD.Core
 {
     public class EmailConfig
     {
-        public static readonly string PrimaryColor = "#042769";
+        public static readonly string PrimaryColor = "#007984";
         public static readonly string DangerColor = "#de7272";
         public string ApiKey { get; set; }
         public string SenderName { get; set; }
