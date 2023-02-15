35385428c2f9ef2ee5adaa5fbe056be6d41d1b8f
Ivan Florencio <ivanflorencio@hotmail.com>
2023-02-15 00:35:16 -0300

    NE-70

--- PeD.Core/Models/Roles.cs
+++ PeD.Core/Models/Roles.cs
@@ -9,12 +9,14 @@ namespace PeD.Core.Models
         public const string Suprimento = "Suprimento";
         public const string Fornecedor = "Fornecedor";
         public const string Colaborador = "Colaborador";
+        public const string AnalistaTecnico = "AnalistaTecnico";
 
         protected static List<string> _allRoles = new List<string>
         {
             Administrador,
             User,
             Colaborador,
+            AnalistaTecnico,
             Suprimento,
             Fornecedor
         };
